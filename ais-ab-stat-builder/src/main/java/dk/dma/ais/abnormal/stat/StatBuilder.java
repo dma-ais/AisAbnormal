7a75cbc0fa79066975f87b2d6d25baa2a018a826
Ole Borup <obo@dma.dk>
2013-11-12 14:16:46 +0100

    More framework

---
+++ ais-ab-stat-builder/src/main/java/dk/dma/ais/abnormal/stat/StatBuilder.java
@@ -0,0 +1,94 @@
+/* Copyright (c) 2011 Danish Maritime Authority
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dma.ais.abnormal.stat;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.zip.GZIPInputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.beust.jcommander.Parameter;
+import com.google.inject.Injector;
+
+import dk.dma.ais.reader.AisReader;
+import dk.dma.ais.reader.AisReaders;
+import dk.dma.commons.app.AbstractDaemon;
+
+/**
+ * AIS Abnormal Behavior statistics builder
+ */
+public class StatBuilder extends AbstractDaemon {
+
+    /** The logger */
+    static final Logger LOG = LoggerFactory.getLogger(StatBuilder.class);
+
+    @Parameter(names = "-file", description = "Historical AIS data file (plain text or .gz)", required = true)
+    String file;
+
+    @Parameter(names = "-db", description = "Output database")
+    String db = "ais-db-stat_db";
+
+    private volatile PacketHandler handler;
+    private volatile AisReader reader;
+
+    @Override
+    protected void runDaemon(Injector injector) throws Exception {
+        LOG.info("StatBuilder starting using file " + file);
+        handler = new PacketHandler(db);
+
+        // Open input stream
+        InputStream input = new FileInputStream(file);
+        if (file.endsWith(".gz")) {
+            LOG.info("Using GZip decompression");
+            input = new GZIPInputStream(input);
+        }
+        
+        // Create reader
+        reader = AisReaders.createReaderFromInputStream(input);
+        reader.registerPacketHandler(handler);
+
+        reader.start();
+        reader.join();
+    }
+
+    @Override
+    public void shutdown() {
+        LOG.info("StatBuilder shutting down");
+        AisReader r = reader;
+        PacketHandler h = handler;
+        if (r != null) {
+            r.stopReader();
+        }
+        if (h != null) {
+            h.getBuildStats().log(true);
+            h.cancel();
+        }
+        super.shutdown();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                LOG.error("Uncaught exception in thread " + t.getClass().getCanonicalName() + ": " + e.getMessage(), e);
+                System.exit(-1);
+            }
+        });
+        new StatBuilder().execute(args);
+    }}
---
+++ ais-ab-stat-builder/src/main/java/dk/dma/ais/abnormal/stat/StatBuilderStatistics.java
@@ -0,0 +1,127 @@
+/* Copyright (c) 2011 Danish Maritime Authority
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dma.ais.abnormal.stat;
+
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class for holding information on the file processing process
+ */
+public class StatBuilderStatistics {
+
+    static final Logger LOG = LoggerFactory.getLogger(StatBuilderStatistics.class);
+
+    static final long DEFAULT_LOG_INTERVAL = 60 * 1000; // 1 minute
+
+    private final long logInterval;
+    private final long startTime = System.currentTimeMillis();
+
+    private long packetCount;
+    private long messageCount;
+    private long posMsgCount;
+    private long statMsgCount;
+    private long cellCount;
+
+    private long lastLog;
+
+    public StatBuilderStatistics() {
+        this.logInterval = DEFAULT_LOG_INTERVAL;
+    }
+
+    public StatBuilderStatistics(long interval, TimeUnit unit) {
+        this.logInterval = unit.toMillis(interval);
+    }
+
+    public void incPacketCount() {
+        packetCount++;
+    }
+
+    public void incMessageCount() {
+        messageCount++;
+    }
+
+    public void incPosMsgCount() {
+        posMsgCount++;
+    }
+
+    public void incStatMsgCount() {
+        statMsgCount++;
+    }
+    
+    public long getPacketCount() {
+        return packetCount;
+    }
+
+    public long getMessageCount() {
+        return messageCount;
+    }
+
+    public long getPosMsgCount() {
+        return posMsgCount;
+    }
+
+    public long getStatMsgCount() {
+        return statMsgCount;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+    
+    public long getLastLog() {
+        return lastLog;
+    }
+    
+    public long getCellCount() {
+        return cellCount;
+    }
+    
+    public void setCellCount(long cellCount) {
+        this.cellCount = cellCount;
+    }
+
+    public double getMessageRate() {
+        double secs = (double)(System.currentTimeMillis() - startTime) / 1000.0;
+        return (double) messageCount / secs;
+    }
+    
+    public void log(boolean force) {
+        if (logInterval <= 0) {
+            return;
+        }
+        long now = System.currentTimeMillis();
+        if (!force && (now - lastLog < logInterval)) {
+            return;
+        }
+        LOG.info("==== Stat build statistics ====");
+        LOG.info(String.format("%-20s %9d", "Pakcet count", packetCount));
+        LOG.info(String.format("%-20s %9d", "Message count", messageCount));
+        LOG.info(String.format("%-20s %9d", "Pos message count", posMsgCount));
+        LOG.info(String.format("%-20s %9d", "Stat message count", statMsgCount));
+        LOG.info(String.format("%-20s %9d", "Cell count", cellCount));
+        LOG.info(String.format("%-20s %9.0f msg/sec", "Message rate", getMessageRate()));
+        LOG.info("==== Stat build statistics ====");
+        lastLog = now;
+    }
+
+    public void log() {
+        log(false);
+    }
+
+}
