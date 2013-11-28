c1f528a766e566e4f9763703d8325de80f287423
Thomas Borg Salling <tbsalling@tbsalling.dk>
2013-11-28 22:04:46 +0100

    Slightly refactored as preparation for loading cell coords by JSON (AISAB-20).

--- ais-ab-web/src/main/webapp/js/dma/abnormal.js
+++ ais-ab-web/src/main/webapp/js/dma/abnormal.js
@@ -1,10 +1,13 @@
 var dmaAbnormalApp = {
-
     map: null,
-
     isGridLayerVisible: false,
+    projectionWGS84: null,
+    projectionSphericalMercator: null,
 
     init: function() {
+        this.projectionWGS84 = new OpenLayers.Projection("EPSG:4326");
+        this.projectionSphericalMercator = new OpenLayers.Projection("EPSG:900913");
+
         map = new OpenLayers.Map("map");
         //map.addLayer(new OpenLayers.Layer.OSM("map", "http://homer/osmtiles/${z}/${x}/${y}.png"));
         map.addLayer(new OpenLayers.Layer.OSM());
@@ -18,15 +21,11 @@ var dmaAbnormalApp = {
     },
 
     zoomToDenmark: function() {
-        // http://dev.openlayers.org/apidocs/files/OpenLayers/Projection-js.html
-        var fromProjection = new OpenLayers.Projection("EPSG:4326"); // transform from WGS 1984
-        var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-
         // set the map centre on the middle of Denmark
         var lat = 56;
         var lon = 12;
         var zoom = 7;
-        map.setCenter(new OpenLayers.LonLat(lon,lat).transform(fromProjection,toProjection), zoom)
+        map.setCenter(new OpenLayers.LonLat(lon,lat).transform(this.projectionWGS84,this.projectionSphericalMercator), zoom)
     },
 
     registerZoomEventHandler: function() {
@@ -88,70 +87,53 @@ var dmaAbnormalApp = {
     },
 
     addGridToLayer: function(layer) {
-        var point = new OpenLayers.Geometry.Point(1335709.8481391, 7558410.9340409);
-
-        // create a line feature from a list of points
-        var pointList = [];                                                              /*
-         * Green style
-         */
-        var style_green = {
-            strokeColor: "#00FF00",
-            strokeWidth: 3,
-            strokeDashstyle: "dashdot",
-            pointRadius: 6,
-            pointerEvents: "visiblePainted",
-            title: "this is a green line"
-        };
-
-        var newPoint = point;
-        for(var p=0; p<15; ++p) {
-            newPoint = new OpenLayers.Geometry.Point(newPoint.x + Math.random(1)*1e5,
-                newPoint.y + Math.random(1)*1e5);
-            pointList.push(newPoint);
-        }
-        pointList.push(new OpenLayers.Geometry.Point(1335709.8481391, 7558410.9340409));
-        var lineFeature = new OpenLayers.Feature.Vector(
-            new OpenLayers.Geometry.LineString(pointList),null,style_green);
-
-        layer.addFeatures([lineFeature]);
-
-        var cell = new OpenLayers.Bounds(1335709.8481391,7558410.9340409,1385709.8481391,7588410.9340409).toGeometry();
-        layer.addFeatures([cell]);
-        dmaAbnormalApp.addCells(layer);
+        var cells = dmaAbnormalApp.loadCells(layer);
+        dmaAbnormalApp.addCells(layer, cells);
     },
 
-    addCells: function(layer) {
-        console.log("Adding cells");
+    loadCells: function() {
+        // http://localhost:8080/abnormal/featuredata/cell?north=55&east=11&south=54.91&west=10.91
+        var cells = new Array();
+
         for (var lon = 12.0; lon < 12.50; lon += 0.05) {
             for (var lat = 56.0; lat < 56.50; lat += 0.05) {
-                var north = lat;
-                var east  = lon;
-                var south = lat - 0.049;
-                var west  = lon - 0.049;
-                console.log("added cell(layer," + north + "," + east + "," + south + "," + west);
-                dmaAbnormalApp.addCell(layer, north, east, south, west);
+                var cell = {
+                    north: lat,
+                    east:  lon,
+                    south: lat - 0.049,
+                    west:  lon - 0.049
+                }
+                cells.push(cell);
             }
+        }
+        return cells;
+    },
+
+    addCells: function(layer, cells) {
+        console.log("Adding cells");
+        for (c in cells) {
+            var cell = cells[c];
+            dmaAbnormalApp.addCell(layer, cell.north, cell.east, cell.south, cell.west);
         }
     },
 
     addCell: function(layer, north, east, south, west) {
-        // http://localhost:8080/abnormal/featuredata/cell?north=55&east=11&south=54.91&west=10.91
         var cellCoords = new Array();
 
         point = new OpenLayers.Geometry.Point(west, north);
-        point.transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+        point.transform(dmaAbnormalApp.projectionWGS84, map.getProjectionObject());
         cellCoords.push(point);
 
         point = new OpenLayers.Geometry.Point(east, north);
-        point.transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+        point.transform(dmaAbnormalApp.projectionWGS84, map.getProjectionObject());
         cellCoords.push(point);
 
         point = new OpenLayers.Geometry.Point(east, south);
-        point.transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+        point.transform(dmaAbnormalApp.projectionWGS84, map.getProjectionObject());
         cellCoords.push(point);
 
         point = new OpenLayers.Geometry.Point(west, south);
-        point.transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+        point.transform(dmaAbnormalApp.projectionWGS84, map.getProjectionObject());
         cellCoords.push(point);
 
         var cellStyle = {
