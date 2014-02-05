230a566758bc457c20477ee17ade0ce0b8b94fce
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-02-05 08:13:34 +0100

    More code to debug via PhantomJS on CloudBees (AISAB-56).

--- ais-ab-web/src/test/java/dk/dma/ais/abnormal/web/FeatureDataIT.java
+++ ais-ab-web/src/test/java/dk/dma/ais/abnormal/web/FeatureDataIT.java
@@ -23,6 +23,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
@@ -50,44 +51,58 @@ public class FeatureDataIT {
         Thread.sleep(1000);
         IntegrationTestHelper.takeScreenshot(browser, "init");
 
-        checkFeatureSetMetadata();
-        zoomInUntilCellsLoadedAndDisplayed();
-        clickOnACell();
+        try {
+            checkFeatureSetMetadata();
+            zoomInUntilCellsLoadedAndDisplayed();
+            clickOnACell();
+        } catch(WebDriverException e) {
+            IntegrationTestHelper.takeScreenshot(browser, "error");
+        }
     }
 
     private void checkFeatureSetMetadata() {
         browser.findElement(By.id("ui-id-2")).click();
         assertEquals("200 m", browser.findElement(By.cssSelector("div#gridsize.useroutput span.data")).getText());
         assertEquals("10 secs", browser.findElement(By.cssSelector("div#downsampling.useroutput span.data")).getText());
+        IntegrationTestHelper.takeScreenshot(browser, "metadata");
     }
 
     private void clickOnACell() throws InterruptedException {
         if (browser instanceof JavascriptExecutor) {
             ((JavascriptExecutor)browser).executeScript("mapModule.map.setCenter(new OpenLayers.LonLat(12.65,56.035).transform(new OpenLayers.Projection(\"EPSG:4326\"), new OpenLayers.Projection(\"EPSG:900913\")), 16)");
+            Thread.sleep(1000);
         }
-        Thread.sleep(1000);
 
         WebElement map = getMap();
 
         Actions actions = new Actions(browser);
-        actions.moveToElement(map);
+        actions.moveToElement(map,map.getSize().getWidth()/2,map.getSize().getHeight()/2);
         actions.click();
         actions.perform();
         Thread.sleep(2500);
-
         // Assert user output
         browser.findElement(By.id("ui-id-1")).click();
         assertEquals("(56°02'17.3\"N, 12°38'26.4\"E)\n(56°01'54.7\"N, 12°39'33.6\"E)", browser.findElement(By.cssSelector("div#viewport.useroutput p")).getText());
-        assertEquals("(56°02'17.3\"N, 12°38'26.4\"E)", browser.findElement(By.cssSelector("div#cursorpos.useroutput p")).getText());
+        // Cursor position is exactly in the center of the map
+        assertEquals("(56°02'06\"N, 12°39'00\"E)", browser.findElement(By.cssSelector("div#cursorpos.useroutput p")).getText());
 
         // Assert feature data
         IntegrationTestHelper.takeScreenshot(browser, "debug1");
-        wait.until(ExpectedConditions.textToBePresentInElement(By.cssSelector("div.cell-data-contents > h5"), "Cell id 6249703285 (56°02'19.4\"N,12°38'21.5\"E) - (56°02'13\"N,12°38'28\"E)"));
+        final String expectedCellInfo = "Cell id 6249302540 (56°02'06.5\"N,12°38'53.8\"E) - (56°02'00\"N,12°39'00.3\"E)";
+        By actualCellInfoElement = By.cssSelector("div.cell-data-contents > h5");
+        wait.until(ExpectedConditions.textToBePresentInElement(actualCellInfoElement, expectedCellInfo));
         IntegrationTestHelper.takeScreenshot(browser, "debug2");
     }
 
     private static WebElement getMap() {
-        return browser.findElement(By.cssSelector("svg#OpenLayers_Layer_Vector_27_svgRoot"));
+        // WebElement map = browser.findElement(By.cssSelector("svg#OpenLayers_Layer_Vector_27_svgRoot"));
+        WebElement map = browser.findElement(By.cssSelector("div#map"));
+        System.out.println("getMap() found map at " + map.getLocation() + ", size " + map.getSize());
+        assertEquals(55, map.getLocation().getX());
+        assertEquals(169, map.getLocation().getY());
+        assertEquals(870, map.getSize().getWidth());
+        assertEquals(524, map.getSize().getHeight());
+        return map;
     }
 
     private void zoomInUntilCellsLoadedAndDisplayed() throws InterruptedException {
--- ais-ab-web/src/test/java/dk/dma/ais/abnormal/web/IntegrationTestHelper.java
+++ ais-ab-web/src/test/java/dk/dma/ais/abnormal/web/IntegrationTestHelper.java
@@ -24,11 +24,16 @@ import java.io.File;
 public class IntegrationTestHelper {
 
     private final static long runNo = System.currentTimeMillis();
+    private final static File screenShotParentDir;
+    static {
+        screenShotParentDir = new File(System.getProperty("user.dir") + File.separator + "target" + File.separator + "selenium");
+        screenShotParentDir.mkdirs();
+    }
 
     public static void takeScreenshot(WebDriver browser, String name) {
         if (browser instanceof TakesScreenshot) {
             File tempfile = ((TakesScreenshot) browser).getScreenshotAs(OutputType.FILE);
-            File dumpfile = new File(System.getProperty("user.dir") + File.separator + "target" + File.separator + "selenium-scrshot-" + runNo + "-" + name + ".png");
+            File dumpfile = new File(screenShotParentDir.getAbsolutePath() + File.separator  + "scrshot-" + runNo + "-" + name + ".png");
             tempfile.renameTo(dumpfile);
             System.err.println("Screen shot dumped to: " + dumpfile.getAbsolutePath());
         } else {
