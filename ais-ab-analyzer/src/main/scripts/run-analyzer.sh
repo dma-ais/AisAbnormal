39b6d2bb2cbfc0a04a6e589cb43a5ce2b9c06ba4
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-06-17 08:50:18 +0200

    Prepare launch script to run with live data (AISAB-145)

--- ais-ab-analyzer/src/main/scripts/run-analyzer.sh
+++ ais-ab-analyzer/src/main/scripts/run-analyzer.sh
@@ -8,4 +8,4 @@ java -jar ais-ab-analyzer-0.1-SNAPSHOT.jar \
 -eventDataDbPassword "replace_with_secret" \
 -eventDataDbName abnormal \
 -statistics ~/AisAbnormal/data/2013H2-grid200-down10.MapDB-1_0_1 \
--aisDataSourceURL tcp://10.33.128.140:65261
+-aisDataSourceURL tcp://10.33.128.144:65261
