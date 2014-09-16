7187bae2d5ed572e78655b1baafa5cc55511ad4c
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-09-16 09:22:00 +0200

    Altered so all appl configuration comes from config file instead of command line.

--- ais-ab-analyzer/src/main/scripts/run-analyzer.bat
+++ ais-ab-analyzer/src/main/scripts/run-analyzer.bat
@@ -1,9 +1,5 @@
 @ECHO off
 ECHO Starting AIS Abnormal event analyzer application
 
- java -jar ais-ab-analyzer-0.1-SNAPSHOT.jar^
-  -downsampling 2^
-  -eventDataRepositoryType h2^
-  -eventDataDbFile events^
-  -statistics stats-2013-06-dwn5-grid200^
-  -aisDataSourceURL file://../../../../../data/ais/aisdk/2013-06/*.txt.gz
+java -jar ais-ab-analyzer-0.1-SNAPSHOT.jar -config %1
+
--- ais-ab-analyzer/src/main/scripts/run-analyzer.sh
+++ ais-ab-analyzer/src/main/scripts/run-analyzer.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-java -jar ais-ab-analyzer-0.1-SNAPSHOT.jar \
--downsampling 2 \
--eventDataRepositoryType pgsql \
--eventDataDbHost $1 \
--eventDataDbPort $2 \
--eventDataDbUsername $3 \
--eventDataDbPassword $4 \
--eventDataDbName $5 \
--statistics $6 \
--aisDataSourceURL $7
+java -jar ais-ab-analyzer-0.1-SNAPSHOT.jar -config $1
