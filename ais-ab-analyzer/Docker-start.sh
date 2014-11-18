125ff5cbb8ee73441ff6085bf0d14d61ef97665d
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-11-18 12:43:16 +0100

    Revert last commit.

--- ais-ab-analyzer/Docker-start.sh
+++ ais-ab-analyzer/Docker-start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /ais-ab-analyzer-0.1-SNAPSHOT
-./run-analyzer.sh $CONFIG_FILE $LOG4J_CONF
+./run-analyzer.sh $CONFIG_FILE
--- ais-ab-analyzer/src/main/scripts/run-analyzer.bat
+++ ais-ab-analyzer/src/main/scripts/run-analyzer.bat
@@ -1,5 +1,5 @@
 @ECHO off
 ECHO Starting AIS Abnormal event analyzer application
 
-java -Dlog4j.configuration %2 -jar ais-ab-analyzer-0.1-SNAPSHOT.jar -config %1
+java -jar ais-ab-analyzer-0.1-SNAPSHOT.jar -config %1
 
